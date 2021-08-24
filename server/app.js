808c91d9b36da062f2abc825e76e8c419f8f7790
George Falkovich <gfalk85@gmail.com>
2021-08-24 20:02:54 +0300

    after merge

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 app.use(express.json({ extended: true }));
+
 app.use(cors());
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/link", require("./routes/link.routes"));
