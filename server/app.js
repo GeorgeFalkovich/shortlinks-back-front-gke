37d93082145e59084e809885c376aa38a8d71b34
George Falkovich <gfalk85@gmail.com>
2021-08-24 19:47:10 +0300

    ds

--- server/app.js
+++ server/app.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const config = require("config");
+const path = require("path");
 const mongoose = require("mongoose");
 const cors = require("cors");
+
 const app = express();
 
 app.use(express.json({ extended: true }));
-app.use(cors());
+app.use(corse());
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/link", require("./routes/link.routes"));
 app.use("/t", require("./routes/redirect.routes"));
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use("/", express.static(path.join(__dirname, "client", "build")));
+
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 const PORT = process.env.PORT || 5000;
 
